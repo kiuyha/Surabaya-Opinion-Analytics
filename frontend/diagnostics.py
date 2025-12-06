a12fad1a294df907d76db1b6d3148db61f559364
kiuyha <ketutshridhara@gmail.com>
2025-12-06 08:33:20 +0700

    update the frontend

--- frontend/diagnostics.py
+++ frontend/diagnostics.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit.components.v1 import html
+import pandas as pd
 
 def show():
     st.header("Model Performance Page")
@@ -9,4 +10,29 @@ def show():
             html(f.read(), height=500, scrolling=True)
             
         with open("visualizations/plot_clusters.html", "r", encoding='utf-8') as f:
+            html(f.read(), height=500, scrolling=True)
+    
+    with st.expander("NER Model Performance", expanded=True):
+        with st.container(border=True):
+            st.subheader("Training Logs")
+            view_type = st.pills(
+                "Chart Type",
+                options=["Plot", "Table"],
+                default="Plot",
+                selection_mode="single",
+                # key=f"view_{}",
+                label_visibility="collapsed"
+            )
+
+            if view_type == 'Table':
+                st.dataframe(
+                    pd.read_csv('visualizations/training_logs.csv')
+                )
+            else:
+                with open("visualizations/ner_train_curve.html", "r", encoding='utf-8') as f:
+                    html(f.read(), height=500, scrolling=True)
+
+        with open("visualizations/ner_classification_report.html", "r", encoding='utf-8') as f:
+            html(f.read(), height=500, scrolling=True)
+        with open("visualizations/ner_confusion_matrix.html", "r", encoding='utf-8') as f:
             html(f.read(), height=500, scrolling=True)
